41b5c3a201437dc1dbc137b1a5b65c5b122064b5
hedaprakash <prakash@starinnovative.com>
2016-07-22 21:27:27 -0700

    v2.1

---
+++ Scripts/ExamplesToExecute.ps1
@@ -0,0 +1,41 @@
+﻿$smtpserver="sfsmtp.sqlfeatures.local" # Change this with your own SMTP address
+$SqlPassword = "Sequoia2012"
+$SqlUser="SA"
+$emailToSent="sqlfeatures@testemail.com" 
+cls
+
+#SQL 2008 STD/ Named Instance / Windows Authentication 
+\\w12r2hv\SQLSetup\Scripts\SQLHealthCheck\SQLHealthCheck.ps1  `
+    -EmailSender $emailToSent `
+    -ServerInstance "AALLINONEE1\SQL_2008_STD" `
+    -smtpserver $smtpserver
+
+#SQL 2008 R2 ENT/ Named Instance / SQL Authentication 
+\\w12r2hv\SQLSetup\Scripts\SQLHealthCheck\SQLHealthCheck.ps1     `
+    -EmailSender $emailToSent  `
+    -ServerInstance "AALLINONEE1\SQL_2008R2_ENT"  `
+    -SqlUser $SqlUser  `
+    -SqlPassword $SqlPassword   `
+    -smtpserver $smtpserver
+
+#SQL 2012 Ent/ Default instance/ SQL Authentication 
+\\w12r2hv\SQLSetup\Scripts\SQLHealthCheck\SQLHealthCheck.ps1     `
+    -EmailSender $emailToSent  `
+    -ServerInstance "w12s12"  `
+    -SqlUser $SqlUser  `
+    -SqlPassword $SqlPassword  `
+    -smtpserver $smtpserver
+
+#SQL 2014 Ent/ Default instance / Windows Authentication / FQDN 
+\\w12r2hv\SQLSetup\Scripts\SQLHealthCheck\SQLHealthCheck.ps1  `
+    -EmailSender $emailToSent  `
+    -ServerInstance "w12r2s14.sqlfeatures.local"  `
+    -smtpserver $smtpserver
+
+#SQL 2016 STD/ Default instance / SQL Authentication / FQDN 
+\\w12r2hv\SQLSetup\Scripts\SQLHealthCheck\SQLHealthCheck.ps1     `
+    -EmailSender $emailToSent  `
+    -ServerInstance "w12r2s16"  `
+    -SqlUser $SqlUser  `
+    -SqlPassword $SqlPassword  `
+    -smtpserver $smtpserver
